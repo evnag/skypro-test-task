62a3c89f1b422913a256ea2cfed10a0ec00d2c94
evnag <vskonev22@gmail.com>
2023-03-28 20:15:49 +0300

    Initial commit

---
+++ src/main/java/com/example/skyprotesttask/SkyproTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.skyprotesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproTestTaskApplication.class, args);
+	}
+
+}
